4af800a0058554f581edf9d40bc1a6399766be22
MiwaUS7605 <minhquang9802@gmail.com>
2022-11-19 23:53:17 +0700

    feat(upgrade): implement editSlang & searchSlang

--- PA_Dict_20127605/Program.java
+++ PA_Dict_20127605/Program.java
@@ -12,6 +12,12 @@ public class Program {
         try {
             SlangDictionary sd = new SlangDictionary();
             System.out.println(sd.loadSlangDictionary("slang.txt"));
+            // System.out.println(sd.addSlang("UwU", "Satisfy", 1));
+            // System.out.println(sd.editSlang("word", "UwU"));
+            // System.out.println(sd.searchSlang("word", "OwO"));
+            // System.out.println(sd.searchSlang("word", "UwU"));
+            System.out.println(sd.searchSlang("definition", ":)"));
+
         }
         catch (Exception exc) {
             System.out.println(exc.toString());
