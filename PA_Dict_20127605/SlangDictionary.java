0c2153fa75da5072add1d07e38bc57ed4df5a054
MiwaUS7605 <minhquang9802@gmail.com>
2022-11-24 00:12:06 +0700

    fix(refractor): move in some function + clean code

--- PA_Dict_20127605/SlangDictionary.java
+++ PA_Dict_20127605/SlangDictionary.java
@@ -4,9 +4,10 @@ import java.util.*;
 public class SlangDictionary {
     //HashMap implementation
     //Each element is a <key,values> equivalent to <slang word, its meaning>
-    private HashMap<String, ArrayList<String>> sDict = new HashMap<String, ArrayList<String>>();
-    private ArrayList<String> logs = new ArrayList<String>();
+    private HashMap<String, ArrayList<String>> sDict;
+    private ArrayList<String> logs;
     private ArrayList<String> keys;
+
     //Search by slang word, or by definition
     //Returns a string contain keys
     public ArrayList<String> searchSlang(String _method, String _info) {
@@ -36,13 +37,32 @@ public class SlangDictionary {
     }
 
     //Load the logs file
-    public Boolean loadLogs(String filename) {
-        return false;
+    public ArrayList<String> loadLogs(String filename) {
+        ArrayList<String> curLogs = new ArrayList<String>();
+        try {
+            FileReader fr = new FileReader(filename);
+            BufferedReader br = new BufferedReader(fr);
+    
+            String line_info = br.readLine();
+
+            while (line_info != null) {
+                curLogs.add(line_info);
+                line_info = br.readLine();
+            }
+            br.close();
+            fr.close();
+        }
+        catch (IOException exc) {
+            System.out.println(exc.toString());
+        }
+        return curLogs;
     }
 
     //Load the slang dictionary file
-    public Boolean loadSlangDictionary(String filename) throws IOException {
+    public Boolean loadSlangDictionary(String filename) {
         try {
+            this.sDict.clear();
+            this.sDict = new HashMap<String, ArrayList<String>>();
             FileReader fr = new FileReader(filename);
             BufferedReader br = new BufferedReader(fr);
             String line_info = br.readLine();
@@ -69,10 +89,10 @@ public class SlangDictionary {
                 this.sDict.put(key,defi);
                 line_info = line_info_next;
             }
-            fr.close();
             br.close();
+            fr.close();
 
-            keys = new ArrayList<String>(this.sDict.keySet());
+            this.keys = new ArrayList<String>(this.sDict.keySet());
             return true;
         }
         catch (IOException exc) {
@@ -83,7 +103,18 @@ public class SlangDictionary {
     }
 
     //Save the logs file
-    public Boolean saveLogs(String filename) {
+    public Boolean saveLogs(String filename, String NEW_CHANGE_LOG) {
+        try {
+            FileWriter fw = new FileWriter(filename, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(NEW_CHANGE_LOG + "\n");
+            bw.close();            
+            fw.close();
+        }
+        catch (IOException exc) {
+            System.out.println(exc.toString());
+        }
+
         return false;
     }
 
@@ -106,6 +137,7 @@ public class SlangDictionary {
                 bw.write(NEWLINE);
             }
             bw.close();            
+            fw.close();
         }
         catch (IOException exc) {
             System.out.println(exc.toString());
@@ -115,13 +147,14 @@ public class SlangDictionary {
     }
 
     //Show the searching history by user
-    public void showHistory() {
-        //Do something with the history (this.logs)
+    public ArrayList<String> showHistory() {
+        return this.logs;
     } 
 
     public Boolean isExist(String word) {
         return this.sDict.containsKey(word);
     }
+
     //Add the slang word to the dictionary
     //isComfirm values: -1: cancel, 0: overwrite/normal adding if not exist, 1: duplicate
     //Return values: true: add success, false: add failure
@@ -148,34 +181,41 @@ public class SlangDictionary {
     }
 
     //Edit the slang word or its definition
-    public Boolean editSlang(String editBy, String _slangWord, String _editText) {
+    public Boolean editSlang(String editBy, String _slangWord, String _editText, int _idx) {
+        //Save a copy definition
+        ArrayList<String> defi = this.sDict.get(_slangWord);
+        if (defi == null) {
+            return false;
+        }
+
         if (editBy.compareTo("word") == 0) {
-            ArrayList<String> defi = this.sDict.get(_slangWord);
-            if (defi == null) {
-                return false;
-            }
-            this.deleteSlang(_slangWord, true);
-            //This should let the user input the word
+            this.deleteSlang(_slangWord, 0);
             this.sDict.put(_editText,defi);
-            //
             return true;
         }
         else if (editBy.compareTo("definition") == 0) {
             //The user choose which definition needs to be edited
+            this.deleteSlang(_slangWord, 0);
+
+            defi.set(_idx,_editText);
+            this.sDict.put(_slangWord,defi);
+            return true;
+
         }
         return false;
     } 
 
     //Delete the slang word
-    public Boolean deleteSlang(String word, Boolean isComfirm) {
-        if (!isComfirm) {return false;}
+    public Boolean deleteSlang(String word, int isComfirm) {
+        if (isComfirm == 1) {return false;}
         this.sDict.remove(word);
         return true;
     }
 
     //Reset current dictionary to default
-    public void resetDictionary() {
+    public void resetDictionary() throws IOException {
         //Finds the original slang dictionary file, update the current ones
+        this.loadSlangDictionary("slang.txt");
     }
     
     //Getting a random slang in dictionary and its definition
@@ -193,19 +233,72 @@ public class SlangDictionary {
     //Getting 4 random slang in dictionary and its definition
     public ArrayList<ArrayList<String>> get4Slangs() {
         ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
-        ArrayList<String> slg1 = this.getRandomSlang();
-        ArrayList<String> slg2 = this.getRandomSlang();
-        ArrayList<String> slg3 = this.getRandomSlang();
-        ArrayList<String> slg4 = this.getRandomSlang();
-        result.add(slg1);
-        result.add(slg2);
-        result.add(slg3);
-        result.add(slg4);
+        int i = 0;
+        while (i++ < 4) {
+            result.add(this.getRandomSlang());
+        }
         return result;
     }
 
     public ArrayList<String> getDefinitionFromSlang(String word) {
         return this.sDict.get(word);
+    }
+
+    public ArrayList<String> showKeys() {
+        //Refresh the keys list
+        this.keys.clear();
+        this.keys = new ArrayList<String>(this.sDict.keySet());
+        return this.keys;
+    }
+
+    public void writeLogs(String CHANGELOG) {
+        this.logs.add(CHANGELOG);
+        this.saveLogs("logs.txt", CHANGELOG);
+    }
+
+    public ArrayList<String> quizBySlangWord() {
+        ArrayList<ArrayList<String>> fourSlang = this.get4Slangs();
+        ArrayList<String> answers = new ArrayList<String>();
+        Random rand = new Random();
+        int randNum = rand.nextInt(4); //is the answer
+        String question = fourSlang.get(randNum).get(0); //get the question
+        
+        for (int i = 0; i < fourSlang.size(); i++) {
+            answers.add(fourSlang.get(i).get(rand.nextInt(fourSlang.get(i).size())));
+        }
+
+        //Set the question and answers
+        ArrayList<String> quiz = new ArrayList<String>();
+        quiz.add(question);
+        quiz.addAll(answers);
+        //
+        return quiz;
+    }
+
+    public ArrayList<String> quizBySlangDefinition() {
+        ArrayList<ArrayList<String>> fourSlang = this.get4Slangs();
+        ArrayList<String> answers = new ArrayList<String>();
+        Random rand = new Random();
+        int randNum1 = rand.nextInt(4); //is the answer to the question
+        int randNum2 = rand.nextInt(fourSlang.get(randNum1).size()); //is the question
+        String question = fourSlang.get(randNum1).get(randNum2);
+        
+        for (int i = 0; i < fourSlang.size(); i++) {
+            answers.add(fourSlang.get(i).get(0));
+        }
+        //Set the question and answers
+        ArrayList<String> quiz = new ArrayList<String>();
+        quiz.add(question);
+        quiz.addAll(answers);
+        //
+        return quiz;
+    }
+
+    SlangDictionary() {
+        this.sDict = new HashMap<String, ArrayList<String>>();
+        this.loadSlangDictionary("slang.txt");
+        this.logs = this.loadLogs("logs.txt");
+        this.keys = new ArrayList<String>(this.sDict.keySet());
     }
 
 }
